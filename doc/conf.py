11716c329bf796f3e92abf1c24de6b73043066e3
Kevin L. Davies <kdavies4@gmail.com>
2012-10-15 17:00:22 -0400

    Fixed formatting issues with units

--- doc/conf.py
+++ doc/conf.py
@@ -126,7 +126,7 @@ html_last_updated_fmt = '%b %d, %Y'
 html_use_smartypants = True
 
 # Custom sidebar templates, maps document names to template names.
-#html_sidebars = {}
+html_sidebars = {'**': ['globaltoc.html', 'searchbox.html']}
 
 # Additional templates that should be rendered to pages, maps page names to
 # template names.
