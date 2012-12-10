773ef9ced6f38c834555bb2ff200595be0ebd83e
Kevin Davies <kdavies4@gmail.com>
2012-12-10 16:06:02 -0500

    Added Travis CI configuration

--- setup.py
+++ setup.py
@@ -5,34 +5,39 @@ See README.txt for instructions.
 """
 
 from distutils.core import setup
+from glob import glob
+
+import modelicares # Only to read the version number
 
 # Install the core Python modules.
 setup(name='ModelicaRes',
-      version='0.5',
+      version=modelicares.__version__,
       author='Kevin Davies',
       author_email='kdavies4@gmail.com',
       #credits=['Kevin Bandy', 'Jason Grout', 'Jason Heeris', 'Joerg Raedler'],
       packages=['control', 'modelicares'],
-      scripts=['bin/loadres'],
+      scripts=glob('bin/*'),
       url='http://kdavies4.github.com/ModelicaRes/',
       license='BSD-compatible (see LICENSE.txt)',
       description='Utilities to set up and analyze Modelica simulation experiments',
       long_description=open('README.txt').read(),
       provides=['modelicares'],
-      requires=['matplotlib', 'numpy', 'wx'],
-      package_dir = {'control': 'external/control/src'},
-      keywords = ['Modelica', 'matplotlib', 'simulation experiment', 'results'],
-      classifiers = ['Development Status :: 4 - Beta',
-                     'Environment :: Console',
-                     'License :: OSI Approved :: BSD License',
-                     'Programming Language :: Python :: 2.7',
-                     'Intended Audience :: Science/Research',
-                     'Topic :: Scientific/Engineering',
-                     'Topic :: Utilities',
-                     ],
+      requires=['python (=2.7)', 'matplotlib', 'numpy', 'wx'],
+      package_dir={'control': 'external/control/src'},
+      keywords=['Modelica', 'matplotlib', 'simulation experiment', 'results'],
+      classifiers=['Development Status :: 4 - Beta',
+                   'Operating System :: POSIX :: Linux',
+                   'Operating System :: Microsoft :: Windows',
+                   'Environment :: Console',
+                   'License :: OSI Approved :: BSD License',
+                   'Programming Language :: Python :: 2.7',
+                   'Intended Audience :: Science/Research',
+                   'Topic :: Scientific/Engineering',
+                   'Topic :: Utilities',
+                   ],
       )
 
-# 10/30/11: Not currently using PyTables (but consider it for the future).
+# 10/30/11: Not currently using PyTables (TODO: Consider it for the future).
 # Install PyTables (version 2.3.1, and maybe later, works).
 # See http://www.pytables.org/moin/Downloads
 #import subprocess
