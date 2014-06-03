b27c5648ff69a16d8c87eb6dd0b477a02521a74c
Kevin Davies <kdavies4@gmail.com>
2014-06-03 05:41:29 -1000

    New layout; fixed sidebar

---
+++ examples2/util-ArrowLine.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+# Example of util.ArrowLine
+
+import matplotlib.pyplot as plt
+from modelicares import util
+
+fig = plt.figure()
+ax = fig.add_subplot(111, autoscale_on=False)
+t = [-1,2]
+s = [0,-1]
+line = util.ArrowLine(t, s, color='b', ls='-', lw=2, arrow='>',
+                      arrowsize=20)
+ax.add_line(line)
+ax.set_xlim(-3, 3)
+ax.set_ylim(-3, 3)
